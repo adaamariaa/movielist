fe8b22c4e8eea39681b842a454758df481961c94
Ada Meriläinen <ada.merilainen@gmail.com>
2023-03-28 13:37:05 +0300

    First commit

---
+++ src/main/java/hh/sof3as3/Movie/MovieApplication.java
@@ -0,0 +1,13 @@
+package hh.sof3as3.Movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/sof3as3/Movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.sof3as3.Movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
